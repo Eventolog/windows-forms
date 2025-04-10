ec4f1c63f92e32a90419818643be0b83ffafbf06
rwxce <148581386+rwxce@users.noreply.github.com>
2025-04-10 17:13:13 +0200

    fix start app

--- Eventology/Program.cs
+++ Eventology/Program.cs
@@ -1,8 +1,4 @@
-﻿using Eventology.Forms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Eventology
@@ -18,7 +14,7 @@ namespace Eventology
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new EventologyForm());
-            Application.Run(new EditSceneryForm());
+            Application.Run(new EventologyForm());
         }
     }
 }
