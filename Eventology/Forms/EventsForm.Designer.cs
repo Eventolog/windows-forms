70f30215ba4dea06d676fe92ce822ccc54b7144d
rwxce <148581386+rwxce@users.noreply.github.com>
2025-05-05 19:42:30 +0200

    add events inital design

--- Eventology/Forms/EventsForm.Designer.cs
+++ Eventology/Forms/EventsForm.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxSearchEvent = new System.Windows.Forms.TextBox();
+            this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.labelEvents = new System.Windows.Forms.Label();
+            this.labelUsers = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
             this.SuspendLayout();
             // 
-            // Events
+            // textBoxSearchEvent
+            // 
+            this.textBoxSearchEvent.Location = new System.Drawing.Point(83, 51);
+            this.textBoxSearchEvent.MaxLength = 0;
+            this.textBoxSearchEvent.Name = "textBoxSearchEvent";
+            this.textBoxSearchEvent.Size = new System.Drawing.Size(282, 20);
+            this.textBoxSearchEvent.TabIndex = 0;
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Location = new System.Drawing.Point(58, 123);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.Size = new System.Drawing.Size(650, 341);
+            this.dataGridViewEvents.TabIndex = 2;
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(748, 123);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.Size = new System.Drawing.Size(268, 341);
+            this.dataGridViewUsers.TabIndex = 3;
+            // 
+            // labelEvents
+            // 
+            this.labelEvents.AutoSize = true;
+            this.labelEvents.BackColor = System.Drawing.Color.Transparent;
+            this.labelEvents.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEvents.ForeColor = System.Drawing.Color.White;
+            this.labelEvents.Location = new System.Drawing.Point(55, 93);
+            this.labelEvents.Name = "labelEvents";
+            this.labelEvents.Size = new System.Drawing.Size(64, 20);
+            this.labelEvents.TabIndex = 4;
+            this.labelEvents.Text = "Events";
+            // 
+            // labelUsers
+            // 
+            this.labelUsers.AutoSize = true;
+            this.labelUsers.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsers.ForeColor = System.Drawing.Color.White;
+            this.labelUsers.Location = new System.Drawing.Point(744, 93);
+            this.labelUsers.Name = "labelUsers";
+            this.labelUsers.Size = new System.Drawing.Size(56, 20);
+            this.labelUsers.TabIndex = 5;
+            this.labelUsers.Text = "Users";
+            // 
+            // EventsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Eventology.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(1065, 505);
-            this.Name = "Events";
+            this.Controls.Add(this.labelUsers);
+            this.Controls.Add(this.labelEvents);
+            this.Controls.Add(this.dataGridViewUsers);
+            this.Controls.Add(this.dataGridViewEvents);
+            this.Controls.Add(this.textBoxSearchEvent);
+            this.MinimumSize = new System.Drawing.Size(0, 100);
+            this.Name = "EventsForm";
             this.Text = "Events";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearchEvent;
+        private System.Windows.Forms.DataGridView dataGridViewEvents;
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.Label labelEvents;
+        private System.Windows.Forms.Label labelUsers;
     }
 }
