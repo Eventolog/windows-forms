5a3131803de62207ad8c0452a5229407901786e2
rwxce <148581386+rwxce@users.noreply.github.com>
2025-05-06 19:35:04 +0200

    add event modal inputs

--- Eventology/Forms/AddEventModal.Designer.cs
+++ Eventology/Forms/AddEventModal.Designer.cs
@@ -29,20 +29,208 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEventModal));
+            this.labelRoom = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxRoom = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelOrganizer = new System.Windows.Forms.Label();
+            this.labelState = new System.Windows.Forms.Label();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.labelInit = new System.Windows.Forms.Label();
+            this.textBoxStartTime = new System.Windows.Forms.TextBox();
+            this.textBoxEndTime = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.textBoxOrganizer = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelRoom
+            // 
+            this.labelRoom.AutoSize = true;
+            this.labelRoom.Location = new System.Drawing.Point(13, 169);
+            this.labelRoom.Name = "labelRoom";
+            this.labelRoom.Size = new System.Drawing.Size(28, 13);
+            this.labelRoom.TabIndex = 18;
+            this.labelRoom.Text = "Sala";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(13, 64);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(57, 13);
+            this.labelDescription.TabIndex = 17;
+            this.labelDescription.Text = "Descripció";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(13, 26);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(29, 13);
+            this.labelName.TabIndex = 16;
+            this.labelName.Text = "Nom";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Enabled = false;
+            this.textBoxDescription.Location = new System.Drawing.Point(100, 64);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(343, 79);
+            this.textBoxDescription.TabIndex = 15;
+            // 
+            // textBoxRoom
+            // 
+            this.textBoxRoom.Enabled = false;
+            this.textBoxRoom.Location = new System.Drawing.Point(100, 166);
+            this.textBoxRoom.Name = "textBoxRoom";
+            this.textBoxRoom.Size = new System.Drawing.Size(185, 20);
+            this.textBoxRoom.TabIndex = 14;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Enabled = false;
+            this.textBoxName.Location = new System.Drawing.Point(100, 23);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(185, 20);
+            this.textBoxName.TabIndex = 13;
+            // 
+            // labelOrganizer
+            // 
+            this.labelOrganizer.AutoSize = true;
+            this.labelOrganizer.Location = new System.Drawing.Point(16, 330);
+            this.labelOrganizer.Name = "labelOrganizer";
+            this.labelOrganizer.Size = new System.Drawing.Size(67, 13);
+            this.labelOrganizer.TabIndex = 26;
+            this.labelOrganizer.Text = "Organitzador";
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Location = new System.Drawing.Point(16, 287);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(31, 13);
+            this.labelState.TabIndex = 25;
+            this.labelState.Text = "Estat";
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Location = new System.Drawing.Point(16, 250);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(29, 13);
+            this.labelEnd.TabIndex = 24;
+            this.labelEnd.Text = "Final";
+            // 
+            // labelInit
+            // 
+            this.labelInit.AutoSize = true;
+            this.labelInit.Location = new System.Drawing.Point(16, 209);
+            this.labelInit.Name = "labelInit";
+            this.labelInit.Size = new System.Drawing.Size(26, 13);
+            this.labelInit.TabIndex = 23;
+            this.labelInit.Text = "Inici";
+            // 
+            // textBoxStartTime
+            // 
+            this.textBoxStartTime.Enabled = false;
+            this.textBoxStartTime.Location = new System.Drawing.Point(100, 206);
+            this.textBoxStartTime.Name = "textBoxStartTime";
+            this.textBoxStartTime.Size = new System.Drawing.Size(150, 20);
+            this.textBoxStartTime.TabIndex = 22;
+            // 
+            // textBoxEndTime
+            // 
+            this.textBoxEndTime.Enabled = false;
+            this.textBoxEndTime.Location = new System.Drawing.Point(100, 247);
+            this.textBoxEndTime.Name = "textBoxEndTime";
+            this.textBoxEndTime.Size = new System.Drawing.Size(150, 20);
+            this.textBoxEndTime.TabIndex = 21;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Enabled = false;
+            this.textBoxStatus.Location = new System.Drawing.Point(100, 284);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStatus.TabIndex = 20;
+            // 
+            // textBoxOrganizer
+            // 
+            this.textBoxOrganizer.Enabled = false;
+            this.textBoxOrganizer.Location = new System.Drawing.Point(100, 323);
+            this.textBoxOrganizer.Name = "textBoxOrganizer";
+            this.textBoxOrganizer.Size = new System.Drawing.Size(173, 20);
+            this.textBoxOrganizer.TabIndex = 19;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(368, 373);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 27;
+            this.buttonSave.Text = "Guardar";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(275, 373);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 28;
+            this.buttonCancel.Text = "Cancelar";
+            this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // AddEventModal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 489);
+            this.ClientSize = new System.Drawing.Size(465, 420);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelOrganizer);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.labelInit);
+            this.Controls.Add(this.textBoxStartTime);
+            this.Controls.Add(this.textBoxEndTime);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.textBoxOrganizer);
+            this.Controls.Add(this.labelRoom);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxRoom);
+            this.Controls.Add(this.textBoxName);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AddEventModal";
             this.Text = "Afegir Event";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelRoom;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxRoom;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelOrganizer;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.Label labelInit;
+        private System.Windows.Forms.TextBox textBoxStartTime;
+        private System.Windows.Forms.TextBox textBoxEndTime;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.TextBox textBoxOrganizer;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
     }
 }
