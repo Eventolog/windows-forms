27326ef4fe50699a93ed408477fd96a8a1d50ff1
joel <email>
2025-05-07 17:55:34 +0200

    add RoomsForm structure

---
+++ Eventology/Forms/RoomsForm/RoomsForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Eventology.Forms.RoomsForm
+{
+    partial class RoomsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelRooms = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddRoom = new System.Windows.Forms.Button();
+            this.btnEditRoom = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelRooms
+            // 
+            this.labelRooms.AutoSize = true;
+            this.labelRooms.BackColor = System.Drawing.Color.Transparent;
+            this.labelRooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRooms.ForeColor = System.Drawing.Color.White;
+            this.labelRooms.Location = new System.Drawing.Point(37, 58);
+            this.labelRooms.Name = "labelRooms";
+            this.labelRooms.Size = new System.Drawing.Size(54, 20);
+            this.labelRooms.TabIndex = 5;
+            this.labelRooms.Text = "Sales";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(461, 341);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btnAddRoom
+            // 
+            this.btnAddRoom.Location = new System.Drawing.Point(532, 96);
+            this.btnAddRoom.Name = "btnAddRoom";
+            this.btnAddRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnAddRoom.TabIndex = 7;
+            this.btnAddRoom.Text = "Afegir";
+            this.btnAddRoom.UseVisualStyleBackColor = true;
+            // 
+            // btnEditRoom
+            // 
+            this.btnEditRoom.Location = new System.Drawing.Point(532, 125);
+            this.btnEditRoom.Name = "btnEditRoom";
+            this.btnEditRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnEditRoom.TabIndex = 8;
+            this.btnEditRoom.Text = "Editar";
+            this.btnEditRoom.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(532, 154);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 9;
+            this.Delete.Text = "Eliminar";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // RoomsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Eventology.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1065, 505);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.btnEditRoom);
+            this.Controls.Add(this.btnAddRoom);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelRooms);
+            this.Name = "RoomsForm";
+            this.Text = "RoomsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRooms;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddRoom;
+        private System.Windows.Forms.Button btnEditRoom;
+        private System.Windows.Forms.Button Delete;
+    }
+}
