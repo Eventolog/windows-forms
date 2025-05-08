7dd479766bfc44d40e0b01bfc7ea53fc5345b9af
rwxce <148581386+rwxce@users.noreply.github.com>
2025-05-08 16:07:36 +0200

    add dashboard labels

--- Eventology/Forms/Main/HomeForm.Designer.cs
+++ Eventology/Forms/Main/HomeForm.Designer.cs
@@ -28,20 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelTitleEvents = new System.Windows.Forms.Label();
+            this.labelTitleUsers = new System.Windows.Forms.Label();
+            this.labelTitleRoomsDisponible = new System.Windows.Forms.Label();
+            this.labelTitleRoomsFull = new System.Windows.Forms.Label();
+            this.labelTitleSoldTickets = new System.Windows.Forms.Label();
+            this.labelEvents = new System.Windows.Forms.Label();
+            this.labelUsers = new System.Windows.Forms.Label();
+            this.labelRoomsDisponible = new System.Windows.Forms.Label();
+            this.labelRoomsFull = new System.Windows.Forms.Label();
+            this.labelSoldTickets = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Home
+            // labelTitleEvents
+            // 
+            this.labelTitleEvents.AutoSize = true;
+            this.labelTitleEvents.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitleEvents.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleEvents.ForeColor = System.Drawing.Color.White;
+            this.labelTitleEvents.Location = new System.Drawing.Point(45, 55);
+            this.labelTitleEvents.Name = "labelTitleEvents";
+            this.labelTitleEvents.Size = new System.Drawing.Size(201, 24);
+            this.labelTitleEvents.TabIndex = 0;
+            this.labelTitleEvents.Text = "Total d\'Esdeveniments";
+            // 
+            // labelTitleUsers
+            // 
+            this.labelTitleUsers.AutoSize = true;
+            this.labelTitleUsers.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitleUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleUsers.ForeColor = System.Drawing.Color.White;
+            this.labelTitleUsers.Location = new System.Drawing.Point(45, 113);
+            this.labelTitleUsers.Name = "labelTitleUsers";
+            this.labelTitleUsers.Size = new System.Drawing.Size(133, 24);
+            this.labelTitleUsers.TabIndex = 1;
+            this.labelTitleUsers.Text = "Total d\'Usuaris";
+            // 
+            // labelTitleRoomsDisponible
+            // 
+            this.labelTitleRoomsDisponible.AutoSize = true;
+            this.labelTitleRoomsDisponible.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitleRoomsDisponible.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleRoomsDisponible.ForeColor = System.Drawing.Color.White;
+            this.labelTitleRoomsDisponible.Location = new System.Drawing.Point(45, 177);
+            this.labelTitleRoomsDisponible.Name = "labelTitleRoomsDisponible";
+            this.labelTitleRoomsDisponible.Size = new System.Drawing.Size(159, 24);
+            this.labelTitleRoomsDisponible.TabIndex = 2;
+            this.labelTitleRoomsDisponible.Text = "Sales Disponibles";
+            // 
+            // labelTitleRoomsFull
+            // 
+            this.labelTitleRoomsFull.AutoSize = true;
+            this.labelTitleRoomsFull.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitleRoomsFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleRoomsFull.ForeColor = System.Drawing.Color.White;
+            this.labelTitleRoomsFull.Location = new System.Drawing.Point(45, 238);
+            this.labelTitleRoomsFull.Name = "labelTitleRoomsFull";
+            this.labelTitleRoomsFull.Size = new System.Drawing.Size(149, 24);
+            this.labelTitleRoomsFull.TabIndex = 3;
+            this.labelTitleRoomsFull.Text = "Sales Ocupades";
+            // 
+            // labelTitleSoldTickets
+            // 
+            this.labelTitleSoldTickets.AutoSize = true;
+            this.labelTitleSoldTickets.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitleSoldTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleSoldTickets.ForeColor = System.Drawing.Color.White;
+            this.labelTitleSoldTickets.Location = new System.Drawing.Point(45, 304);
+            this.labelTitleSoldTickets.Name = "labelTitleSoldTickets";
+            this.labelTitleSoldTickets.Size = new System.Drawing.Size(167, 24);
+            this.labelTitleSoldTickets.TabIndex = 4;
+            this.labelTitleSoldTickets.Text = "Entrades Venudes";
+            // 
+            // labelEvents
+            // 
+            this.labelEvents.AutoSize = true;
+            this.labelEvents.BackColor = System.Drawing.Color.Transparent;
+            this.labelEvents.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEvents.ForeColor = System.Drawing.Color.White;
+            this.labelEvents.Location = new System.Drawing.Point(265, 55);
+            this.labelEvents.Name = "labelEvents";
+            this.labelEvents.Size = new System.Drawing.Size(33, 24);
+            this.labelEvents.TabIndex = 5;
+            this.labelEvents.Text = "A1";
+            // 
+            // labelUsers
+            // 
+            this.labelUsers.AutoSize = true;
+            this.labelUsers.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsers.ForeColor = System.Drawing.Color.White;
+            this.labelUsers.Location = new System.Drawing.Point(265, 113);
+            this.labelUsers.Name = "labelUsers";
+            this.labelUsers.Size = new System.Drawing.Size(33, 24);
+            this.labelUsers.TabIndex = 6;
+            this.labelUsers.Text = "A1";
+            // 
+            // labelRoomsDisponible
+            // 
+            this.labelRoomsDisponible.AutoSize = true;
+            this.labelRoomsDisponible.BackColor = System.Drawing.Color.Transparent;
+            this.labelRoomsDisponible.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoomsDisponible.ForeColor = System.Drawing.Color.White;
+            this.labelRoomsDisponible.Location = new System.Drawing.Point(265, 177);
+            this.labelRoomsDisponible.Name = "labelRoomsDisponible";
+            this.labelRoomsDisponible.Size = new System.Drawing.Size(33, 24);
+            this.labelRoomsDisponible.TabIndex = 7;
+            this.labelRoomsDisponible.Text = "A1";
+            // 
+            // labelRoomsFull
+            // 
+            this.labelRoomsFull.AutoSize = true;
+            this.labelRoomsFull.BackColor = System.Drawing.Color.Transparent;
+            this.labelRoomsFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoomsFull.ForeColor = System.Drawing.Color.White;
+            this.labelRoomsFull.Location = new System.Drawing.Point(265, 238);
+            this.labelRoomsFull.Name = "labelRoomsFull";
+            this.labelRoomsFull.Size = new System.Drawing.Size(33, 24);
+            this.labelRoomsFull.TabIndex = 8;
+            this.labelRoomsFull.Text = "A1";
+            // 
+            // labelSoldTickets
+            // 
+            this.labelSoldTickets.AutoSize = true;
+            this.labelSoldTickets.BackColor = System.Drawing.Color.Transparent;
+            this.labelSoldTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSoldTickets.ForeColor = System.Drawing.Color.White;
+            this.labelSoldTickets.Location = new System.Drawing.Point(265, 304);
+            this.labelSoldTickets.Name = "labelSoldTickets";
+            this.labelSoldTickets.Size = new System.Drawing.Size(33, 24);
+            this.labelSoldTickets.TabIndex = 9;
+            this.labelSoldTickets.Text = "A1";
+            // 
+            // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Eventology.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(1065, 505);
-            this.Name = "Home";
+            this.Controls.Add(this.labelSoldTickets);
+            this.Controls.Add(this.labelRoomsFull);
+            this.Controls.Add(this.labelRoomsDisponible);
+            this.Controls.Add(this.labelUsers);
+            this.Controls.Add(this.labelEvents);
+            this.Controls.Add(this.labelTitleSoldTickets);
+            this.Controls.Add(this.labelTitleRoomsFull);
+            this.Controls.Add(this.labelTitleRoomsDisponible);
+            this.Controls.Add(this.labelTitleUsers);
+            this.Controls.Add(this.labelTitleEvents);
+            this.Name = "HomeForm";
             this.Text = "Home";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitleEvents;
+        private System.Windows.Forms.Label labelTitleUsers;
+        private System.Windows.Forms.Label labelTitleRoomsDisponible;
+        private System.Windows.Forms.Label labelTitleRoomsFull;
+        private System.Windows.Forms.Label labelTitleSoldTickets;
+        private System.Windows.Forms.Label labelEvents;
+        private System.Windows.Forms.Label labelUsers;
+        private System.Windows.Forms.Label labelRoomsDisponible;
+        private System.Windows.Forms.Label labelRoomsFull;
+        private System.Windows.Forms.Label labelSoldTickets;
     }
 }
